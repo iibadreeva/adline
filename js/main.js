bed056e4e396e6d3583e8b323095684a77ead970
iibadreeva <iibadreeva@mail.ru>
2018-09-20 20:41:54 +0300

    new

--- js/main.js
+++ js/main.js
@@ -182,7 +182,47 @@ function appStylesFl(e) {
     this.classList.toggle('open');
 }
 
-
+//button for mobile version
 try{ appStyles.init(); }catch (e){}
 
+//slider main animation
 fncSlider(".example-slider", {autoSlidingDelay: 4000});
+
+
+//mini slider
+const slider = document.querySelector('.js-slider');
+const imgSlider = simpleslider.getSlider({
+    container: slider,
+    prop: 'left',
+    init: -100,
+    show: 0,
+    delay: 3.2,
+    duration: 1,
+    end: 100,
+    unit: '%',
+    ease: bounceOut
+});
+function bounceOut(time, begin, change, duration) {}
+const  left = document.querySelectorAll('.js-slider-link')[1],
+    right = document.querySelectorAll('.js-slider-link')[0];
+let isNext = true;
+
+left.addEventListener('click', function (e) {
+    if (!isNext) {
+        imgSlider.reverse();
+        isNext = true;
+    }
+    imgSlider.next();
+    // imgSlider.pause();
+    e.preventDefault();
+});
+right.addEventListener('click', function (e) {
+    if (isNext) {
+        imgSlider.reverse();
+        isNext = false;
+    }
+    imgSlider.next();
+    // imgSlider.pause();
+    e.preventDefault();
+});
+// imgSlider.pause();
