24c8f13863dbee631d8ac72e0b221975c5ec35eb
iibadreeva <iibadreeva@mail.ru>
2018-09-26 13:41:17 +0300

    app

--- source/js/5_slider-3.js
+++ source/js/5_slider-3.js
@@ -1,7 +1,47 @@
 //mini slider
-const sliderS = document.querySelector('.js-slider');
+const slider = document.querySelector('.js-slider');
+const sliderS = document.querySelector('.js-sliders');
 let isNextS = true;
 
+try{
+    var imgSliderS = simpleslider.getSlider({
+        container: slider,
+        prop: 'opacity',
+        init: '',
+        show: 1,
+        // delay: 3.2,
+        duration: 1,
+        end: 0,
+        unit: '',
+    });
+    imgSliderS.pause();
+
+    var rightS = document.getElementsByClassName('js-slider-link-right');
+    var leftS = document.getElementsByClassName('js-slider-link-left');
+
+    for(var i =0; 0 <= rightS.length; i++){
+        rightS[i].addEventListener('click', function (e) {
+            console.log('right');
+            if (isNextS) {
+                imgSliderS.reverse();
+                isNextS = false;
+            }
+            imgSliderS.next();
+            e.preventDefault();
+            imgSliderS.pause();
+        });
+        leftS[i].addEventListener('click', function (e) {
+            console.log('left');
+            if (!isNextS) {
+                imgSliderS.reverse();
+                isNextS = true;
+            }
+            imgSliderS.next();
+            e.preventDefault();
+            imgSliderS.pause();
+        });
+    }
+}catch (e){}
 try{
     var imgSliderS = simpleslider.getSlider({
         container: sliderS,
@@ -13,6 +53,7 @@ try{
         end: 0,
         unit: '',
     });
+    // imgSliderS.pause();
 
     var rightS = document.getElementsByClassName('js-slider-link-right');
     var leftS = document.getElementsByClassName('js-slider-link-left');
@@ -39,8 +80,4 @@ try{
             // imgSliderS.pause();
         });
     }
-
-
-
-    // imgSliderS.pause();
 }catch (e){}
--- source/js/9_main.js
+++ source/js/9_main.js
@@ -4,36 +4,9 @@ new WOW().init();
 
 $(".js-slick").stick_in_parent();
 
-var  item = '.overlay'
-    ,$item = $(item)
-    ,popup = '.js-popup'
-    ,$popup = $(popup);
 
-$('body').on('click', popup, function(e) {
-    var $modal = $('.popup')
-        ,top =  ($(window).scrollTop() + 50)
-        ,footTop =  $('.footer').offset().top
-        ,tops = footTop-top;
-
-    if( tops < 1000 ) top = top-(tops/2) - 500;
-
-    e.preventDefault();
-
-    $modal.show();
-    $item.show();
-    $modal.css({'top':top});
-
-    // var id = $(this).data('id');
-    var id = $('.popup');
-
-    if ($(id).length != 0) {
-        $('html, body').animate({ scrollTop: $(id).offset().top }, 500);
-    }
-});
-
-$('body').on('click', item, function() {
-    var $modal = $('.popup');
-    $modal.hide();
-    $item.hide();
-    $(this).hide();
+$('.js-sliders').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {enabled:true}
 });
