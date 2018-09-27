ff32bef9b47433f9130d24e88162006880615bc8
iibadreeva <iibadreeva@mail.ru>
2018-09-27 11:36:40 +0300

    app

--- source/js/9_main.js
+++ source/js/9_main.js
@@ -5,8 +5,14 @@ new WOW().init();
 $(".js-slick").stick_in_parent();
 
 
-$('.js-sliders').magnificPopup({
+$('.js-sliders, .js-slider').magnificPopup({
     delegate: 'a',
     type: 'image',
     gallery: {enabled:true}
 });
+
+// $('.simple-slider__inner').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     gallery: {enabled:true}
+// });
