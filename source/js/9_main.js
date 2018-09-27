c5eb3ebaec781e41a6e033f351b7bae427525ab6
iibadreeva <iibadreeva@mail.ru>
2018-09-27 11:45:26 +0300

    app

---
+++ source/js/9_main.js
@@ -0,0 +1,18 @@
+//animate content
+new WOW().init();
+
+
+$(".js-slick").stick_in_parent();
+
+
+$('.js-sliders, .js-slider').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {enabled:true}
+});
+
+// $('.simple-slider__inner').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     gallery: {enabled:true}
+// });
